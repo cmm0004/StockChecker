e793507ac23da37201ff1c1972e1cf183b03698e
Candice Mccollough <candice.mccollough@careerbuilder.com>
2016-05-15 13:02:22 -0400

    fixed

--- lib/endpoints/jobluv.rb
+++ lib/endpoints/jobluv.rb
@@ -1,14 +1,12 @@
-require './lib/helpers/djkhaled_links'
-
 module StockChecker
 	class Application < Sinatra::Base
-		include Helpers::DjKaledLinks
+	
 		
 		post '/jobluv/?' do
 			params = JSON.parse(request.env["rack.input"].read, :symbolize_names => true)
 			incoming_message = params[:item][:message][:message].split(' ')
 			mentioned_user = params[:item][:message][:mentions][0][:mention_name]
-			action = determine_action(incoming_message[-1])
+			action = incoming_message[-1]
 			#possible usages 
 			#/Jobluv <user> <++>
 			#/Jobluv <user> <-->
@@ -16,14 +14,14 @@ module StockChecker
 			if action == '++'
 				return {
 						color: "green",
-						message: DjKaledLinks.get_plus_jobluv_links.sample,
+						message: "#{mentioned_user} #{get_plus_jobluv_links().sample}",
 						notify: false,
 						message_format: "text"
 					}.to_json
 			elsif action == '--'
 				return {
 						color: "red",
-						message: DjKaledLinks.get_minus_jobluv_links.sample,
+						message: "#{mentioned_user} #{get_minus_jobluv_links().sample}",
 						notify: false,
 						message_format: "text"
 					}.to_json
@@ -34,39 +32,24 @@ module StockChecker
 
 		
 
-		post '/portfolio/?' do
-			params = JSON.parse(request.env["rack.input"].read, :symbolize_names => true)
-			incoming_message = params[:item][:message][:message].split(' ')
-			mentioned_user = params[:item][:message][:mentions][0][:mention_name]
-			stock_to_add = incoming_message[-1]
-
-			if incoming_message[1] =~ /addstock/
-				begin
-					StockChecker::Models::Portfolio.create!(
-						hipchat_username: mentioned_user,
-						stock_name: stock_to_add)
-				rescue exception => ex
-					return {
-						color: "red",
-						message: ex,
-						notify: false,
-						message_format: "text"
-					}.to_json
-				end
-
-				user_portfolio = StockChecker::Models::Portfolio.where(hipchat_username: mentioned_user).order(created_at: :asc)
-				stocks = []
-				user_portfolio.each do |stock|
-					stocks << stock.stock_name
-				end
-
-				return {
-						color: "green",
-						message: "Added #{stock_to_add}, #{mentioned_user} 's Portfolio:\n #{stocks.join(",")}",
-						notify: false,
-						message_format: "text"
-					}.to_json 
+		def get_plus_jobluv_links
+				links = [
+					#yousmart
+					'https://i.imgur.com/OfPEnFZ.gif',
+					#champaign on booty
+					'https://m.popkey.co/a39a1a/7zxGZ.gif',
+					#i appreciate you
+					'https://m.popkey.co/ea155c/G0AgD.gif'
+				]
 			end
-		end
+
+			def get_minus_jobluv_links
+				links = [
+					#some people cant handle success
+					'https://m.popkey.co/3060d3/DYgpr.gif',
+					#congrats you PLAYED YOURSELF.
+					'http://i.makeagif.com/media/10-12-2015/mQJF9e.gif'
+				]
+			end
 	end
 end
